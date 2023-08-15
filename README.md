72dd3f42e07c44d160e4edf33eb3bde5eb4b4288
adbenitez <asieldbenitez@gmail.com>
2023-08-15 19:55:57 +0200

    generate site dynamically from json data

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ To see the list of public Delta Chat bots, visit:
 https://deltachat-bot.github.io/public-bots/
 
 If you are a bot administrator and want to add your bot instance to the list,
-clone this repo and edit `index.md` file.
+clone this repo and edit `data.json` file adding your bot metadata.
---
+++ generate_site.py
@@ -0,0 +1,42 @@
+import json
+from argparse import Namespace
+from pathlib import Path
+
+lang2flag = {
+    "en": "🇺🇸",
+    "es": "🇪🇸",
+    "all": "🌐",
+}
+
+
+def main():
+    with Path("data.json").open() as data_file:
+        data = json.load(data_file)
+    with Path("_config.yml").open("w", encoding="utf-8") as jekyll_cfg:
+        jekyll_cfg.write("theme: jekyll-theme-hacker")
+    with Path("index.md").open("w", encoding="utf-8") as page:
+        page.writelines(
+            [
+                "## Public Delta Chat Bots\n\n",
+                "To verify the bot click the bot address in the table below.\n\n",
+                "To see the bot's help try sending `/help` to the bot.\n\n",
+                "| Address | Description | Language | Administrator |\n",
+                "| ------- | ----------- | :------: | ------------- |\n",
+            ]
+        )
+        admins = data.get("admins", {})
+        for bot in sorted(data["bots"], key=lambda bot: bot["addr"]):
+            bot.setdefault("qr", "mailto:" + bot["addr"])
+            bot = Namespace(**bot)
+            bot.lang = lang2flag.get(bot.lang, bot.lang)
+            if bot.admin in admins:
+                admin = Namespace(**admins[bot.admin])
+                if "email" in admin:
+                    bot.admin = f"[{bot.admin}](mailto:{admin.email})"
+            page.write(
+                f"| [{bot.addr}]({bot.qr}) | {bot.description} | {bot.lang} | {bot.admin} |\n"
+            )
+
+
+if __name__ == "__main__":
+    main()
