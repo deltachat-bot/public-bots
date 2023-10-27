58b90e1cd79fbf651a15dc0207020c705410847e
adbenitez <asieldbenitez@gmail.com>
2023-10-27 08:43:54 +0200

    update run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -29,9 +29,7 @@ export PATH="$HOME/.cargo/bin/:$PATH"
 if ! command -v deltachat-rpc-server &> /dev/null
 then
     echo "deltachat-rpc-server not found, installing..."
-    curl -L "https://github.com/deltachat/deltachat-core-rust/releases/download/v1.120.0/deltachat-rpc-server-x86_64" -o ~/.cargo/bin/deltachat-rpc-server
-    chmod +x ~/.cargo/bin/deltachat-rpc-server
-
+    pip install deltachat-rpc-server
 fi
 if ! command -v courtney &> /dev/null
 then
